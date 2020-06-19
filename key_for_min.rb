3c9134bd95d9eadff57dd6af1bfe71ee5f18ad9a
Josue Viramontes <josueviramontes1997@gmail.com>
2020-06-19 07:19:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,9 @@ def key_for_min_value(name_hash)
 lowest_key = nil 
 lowest_value = nil 
     hash.each do |k, v|
-      if lowest_value == nil || value  < lowest_value
-        lowest_value = value 
-        lowest_key = key 
+      if lowest_value == nil || v  < lowest_value
+        lowest_value = v 
+        lowest_key = k
       end 
     end 
     lowest_key
